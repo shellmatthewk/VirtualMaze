a628085095b600f248b8cfdbf282d91153ff7073
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-08-20 00:41:42 +0800

    Updates documentation and places a comment at ScreenSaver

--- Assets/Scripts/ScreenSaver.cs
+++ Assets/Scripts/ScreenSaver.cs
@@ -591,6 +591,7 @@ public class ScreenSaver : BasicGUIController {
         switch (data.dataType) {
             case DataTypes.SAMPLE_TYPE:
                 Fsample fs = (Fsample)data;
+                //This might cause a duplicate of the dataa to be written. Not sure. -Xavier, 19/08/23
                 if (IsInScreenBounds(fs.rawRightGaze)) {
                     gazePointPool?.AddGazePoint(GazeCanvas, viewport, fs.RightGaze);
 
@@ -600,7 +601,7 @@ public class ScreenSaver : BasicGUIController {
                 }
                 else {
                     //ignore if gaze is out of bounds
-                    recorder.IgnoreSample(data.dataType, data.time, fs.rawRightGaze, robot.position, robot.rotation.eulerAngles.y, isLastSampleInFrame);
+                    // recorder.IgnoreSample(data.dataType, data.time, fs.rawRightGaze, robot.position, robot.rotation.eulerAngles.y, isLastSampleInFrame);
                 }
                 return SessionTrigger.NoTrigger;
             case DataTypes.MESSAGEEVENT:
