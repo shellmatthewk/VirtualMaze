126c63b2ff2e5ef615aec54263ea6d0ed0298d5b
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-09-03 14:46:39 +0800

    Apply fix to use viewport space
    Fix is currently bugged, do not use in main branch

--- Assets/Scripts/Raycasting/RangeCorrector.cs
+++ Assets/Scripts/Raycasting/RangeCorrector.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using System;
 using System.Collections.Generic;
@@ -8,40 +10,33 @@ using UnityEngine;
 namespace RangeCorrector{
     public class RangeCorrector {
 
-        private Tuple<double,double,double,double>  originalRange;
+        private Rect  originalRange;
         //x-start, x-end, y-start, y-end
-        private Tuple<double,double,double,double> newRange; // Target range
+        private Rect newRange; // Target range
         //x-start, x-end, y-start, y-end
 
         private static readonly Lazy<RangeCorrector> HD_TO_VIEWPORT_INTERNAL = 
         new Lazy<RangeCorrector>(() => new RangeCorrector(
-            Tuple.Create(0.0d,1920.0d,0.0d,1080.0d),
-            Tuple.Create(0.0d,1.0d,0.0d,1.0d))
+            new Rect(0,0,1920,1080),
+            new Rect(0,0,1,1))
             );
         
+        
+        
+
         public static RangeCorrector HD_TO_VIEWPORT {get {return HD_TO_VIEWPORT_INTERNAL.Value;}}
-        public RangeCorrector(Tuple<double,double,double,double> originalRange,
-                                Tuple<double,double,double,double> newRange) {
-            
-            this.originalRange = originalRange;
-            this.newRange = newRange;
+        
+        public RangeCorrector(Rect orig, Rect newRect) {
+            this.originalRange = orig;
+            this.newRange = newRect;
         }
+        
 
 
 
         public Vector2 correctVector(Vector2 value) {
             
-            // perform intervening calculations in double to try to mitigate errors
-            double normalisedX = ((value.x - originalRange.Item1) / 
-            (originalRange.Item2 - originalRange.Item1)); 
-            double normalisedY = ((value.y - originalRange.Item3) / 
-            (originalRange.Item4 - originalRange.Item3));
-
-            double correctedX = (normalisedX * (newRange.Item2 - newRange.Item1)) + newRange.Item1;
-            double correctedY = (normalisedY * (newRange.Item4 - newRange.Item3)) + newRange.Item3;
-
-            return new Vector2((float) correctedX, (float) correctedY);
-            // lose precision but vector2 is in float so no choice
+            return Rect.NormalizedToPoint(this.newRange,Rect.PointToNormalized(this.originalRange,value));
         }
 
     }
